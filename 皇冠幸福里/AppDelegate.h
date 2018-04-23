3ef37d0939d83fb97b8f9c24c85ab4d407cea50a
YGLee <23801504@qq.com>
2018-04-23 16:45:30 +0800

    First

---
+++ 皇冠幸福里/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  皇冠幸福里
+//
+//  Created by LiYugang on 2018/4/23.
+//  Copyright © 2018年 LiYugang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
